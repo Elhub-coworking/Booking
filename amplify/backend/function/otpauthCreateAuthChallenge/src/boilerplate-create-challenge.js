997b5c47b421a8fb9d9c584a8dfa786e969ddce2
Viviana Benitez <viviana.bernitez@gmail.com>
2023-06-22 10:36:23 -0500

    module import bug fixed. Allowed user can redirect now to enter OTP screen

--- amplify/backend/function/otpauthCreateAuthChallenge/src/boilerplate-create-challenge.js
+++ amplify/backend/function/otpauthCreateAuthChallenge/src/boilerplate-create-challenge.js
@@ -1,13 +1,13 @@
 const AWS = require("aws-sdk");
 
 exports.handler = (event, context, callback) => {
-  //Create a random number for otp
   const challengeAnswer = Math.random().toString(10).slice(2, 6);
   const phoneNumber = event.request.userAttributes.phone_number;
 
   //For Debugging
   console.log(event, context);
 
+  
   //sns sms
   const sns = new AWS.SNS({ region: "us-east-1" });
   sns.publish(
@@ -24,10 +24,12 @@ exports.handler = (event, context, callback) => {
           DataType: "String",
           StringValue: "Transactional",
         },
+        
       },
     },
     function (err, data) {
       if (err) {
+        console.log("CREATE ERROR")
         console.log(err.stack);
         console.log(data);
         return;
